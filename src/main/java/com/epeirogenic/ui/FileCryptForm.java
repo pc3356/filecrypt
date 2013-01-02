2d2e572ef7c0a60665db8d66d330ab758b39fda3
Philip Coates <philip.coates@epeirogenic.com>
2013-01-02 13:30:26 +0000

    Add UI

---
+++ src/main/java/com/epeirogenic/ui/FileCryptForm.java
@@ -0,0 +1,28 @@
+package com.epeirogenic.ui;
+
+import javax.swing.*;
+
+public class FileCryptForm {
+	private JPanel panel;
+	private JPasswordField passwordField;
+	private JTextField outputFileField;
+	private JButton browseInputButton;
+	private JButton browseOutputButton;
+	private JButton encryptButton;
+	private JButton decryptButton;
+	private JLabel inputFileLabel;
+	private JLabel passwordLabel;
+	private JLabel title;
+	private JLabel outputFileLabel;
+	private JTextField inputFileField;
+
+	public FileCryptForm() {
+
+
+	}
+
+	private void createUIComponents() {
+		// TODO: place custom component creation code here
+		panel.setVisible(true);
+	}
+}
