d6b54287545cf8381c82eebc827340b17696b867
pc3356 <philip.coates@epeirogenic.com>
2025-10-14 10:04:54 +0200

    Update Commons Lang Add basic logging control properties Remove dependency on IntelliJ forms

--- src/main/java/com/epeirogenic/filecrypt/EncryptorService.java
+++ src/main/java/com/epeirogenic/filecrypt/EncryptorService.java
@@ -1,41 +1,38 @@
 package com.epeirogenic.filecrypt;
 
-import org.apache.commons.io.FileUtils;
-import org.jasypt.encryption.pbe.PBEByteCleanablePasswordEncryptor;
-import org.jasypt.util.binary.BinaryEncryptor;
-import org.jasypt.util.binary.StrongBinaryEncryptor;
-
 import java.io.File;
+import org.apache.commons.io.FileUtils;
+import org.jasypt.util.binary.StrongBinaryEncryptor;
 
 public class EncryptorService {
 
     public byte[] encrypt(File input, char[] password) throws Exception {
 
-        StrongBinaryEncryptor binaryEncryptor = new StrongBinaryEncryptor();
+        var binaryEncryptor = new StrongBinaryEncryptor();
         binaryEncryptor.setPasswordCharArray(password);
 
-        byte[] fileAsBytes = FileUtils.readFileToByteArray(input);
+        var fileAsBytes = FileUtils.readFileToByteArray(input);
         return binaryEncryptor.encrypt(fileAsBytes);
     }
 
     public void encrypt(File input, File output, char[] password) throws Exception {
 
-        byte[] encryptedBinary = encrypt(input, password);
+        var encryptedBinary = encrypt(input, password);
         FileUtils.writeByteArrayToFile(output, encryptedBinary);
     }
 
     public byte[] decrypt(File input, char[] password) throws Exception {
 
-        StrongBinaryEncryptor binaryEncryptor = new StrongBinaryEncryptor();
+        var binaryEncryptor = new StrongBinaryEncryptor();
         binaryEncryptor.setPasswordCharArray(password);
 
-        byte[] fileAsBytes = FileUtils.readFileToByteArray(input);
+        var fileAsBytes = FileUtils.readFileToByteArray(input);
         return binaryEncryptor.decrypt(fileAsBytes);
     }
 
     public void decrypt(File input, File output, char[] password) throws Exception {
 
-        byte[] decryptedBinary = decrypt(input, password);
+        var decryptedBinary = decrypt(input, password);
         FileUtils.writeByteArrayToFile(output, decryptedBinary);
     }
 }
